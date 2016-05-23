3e1f9d9159da579fc789b70ff72a0b15eccaeca6
Dave Liddament <dave@lampbristol.com>
2016-05-23 12:39:53 +0100

    ADDED code to make test work.
    Next step: Ignore negative numbers (e.g. input of '2,-3,4' outputs 6).

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -25,7 +25,7 @@ class StringCalculator
         }
 
         $result = 0;
-        foreach(explode(',', $string) as $number) {
+        foreach(explode($delimiter, $string) as $number) {
 
             if (!is_numeric($number)) {
                 throw new \InvalidArgumentException("Invalid number [$number] within input [$string].");
